4935d60d9723d5b8ad1291e7b9acefe5320bdc62
Anastasia <119536183+Asterisk2018@users.noreply.github.com>
2022-12-19 13:22:20 +0300

    Покупка билетов

---
+++ main.py
@@ -0,0 +1,26 @@
+y = input("Введите количество билетов:")
+chek = int(y)
+chek_tickets = []
+summ_tickets = []
+age_list = []
+summ1 = 0
+age_summ1 = [0,17]
+summ2 = 990
+age_summ2 = [18,24]
+summ3 = 1390
+age_summ3 = [25,100]
+for i in range(1,chek +1):
+  chek_tickets = list(range(1,chek +1))
+c = int(len(chek_tickets))
+for i in range(c):
+  x = int(input("Введите ваш возраст: "))
+  age_list.append(x)
+if x >= age_summ1[0] and x <= age_summ1[1]:
+  summ_tickets.append(summ1)
+if x >= age_summ2[0] and x <= age_summ2[1]:
+  summ_tickets.append(summ2)
+if x >= age_summ3[0] and x <= age_summ3[1]:
+    summ_tickets.append(summ3)
+if c > 3:
+  print("Сумма вашего заказа:",sum(summ_tickets)*0,9)
+print("Сумма вашего заказа:",sum(summ_tickets))
